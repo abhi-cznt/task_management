b23162382c41ca7ef6f97bdc6cbdb633e5743a23
Akhi <abhi96518756@gmail.com>
2023-07-29 09:15:28 +0530

    created handle requests

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/abhi-cznt/task_management
+
+go 1.20
+
+require github.com/gorilla/mux v1.8.0 // direct
--- src/main.go
+++ src/main.go
@@ -2,9 +2,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
+
+	// "encoding/json"
 	"github.com/gorilla/mux"
 )
 
@@ -37,12 +40,71 @@ func allTasks() {
 	fmt.Println(tasks)
 }
 
-func handleRequest(){
+func homePage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("I am home page")
+
+}
+
+func getTasks(w http.ResponseWriter, r *http.Request) {
+
+	// fmt.Println("I am tasks  page")
+	w.Header().Set("Content-Type", "application/")
+	json.NewEncoder(w).Encode(tasks)
+
+}
+
+func getTask(w http.ResponseWriter, r *http.Request) {
+	// fmt.Println("I am task page")
+
+	taskId := mux.Vars(r)
+	flag := false
+	for i := 0; i < len(tasks); i++ {
+		if taskId["id"] == tasks[i].ID {
+			json.NewEncoder(w).Encode(tasks[i])
+			flag = true
+			break
+		}
+	}
+
+	if flag == false {
+		json.NewEncoder(w).Encode(map[string]string{"status": "error"})
+	}
+
+}
+
+func createTask(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("I am home page")
+
+}
+
+func deleteTask(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("I am home page")
+
+}
+
+func updateTask(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Println("I am home page")
+
+}
+
+func handleRequest() {
 	router := mux.NewRouter()
+	router.HandleFunc("/", homePage).Methods("GET")
+	router.HandleFunc("/gettasks", getTasks).Methods("GET")
+	router.HandleFunc("/gettask/{id}", getTask).Methods("GET")
+	router.HandleFunc("/create", createTask).Methods("POST")
+	router.HandleFunc("/delete/{id}", deleteTask).Methods("DELETE")
+	router.HandleFunc("/update/{id}", updateTask).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8082", router))
+	// log.Fatal(http.ListenAndServe(":55336", router))
 }
 
 func main() {
 	allTasks()
 	fmt.Println("hello ")
+	handleRequest()
 }
